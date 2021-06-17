9d33745e3cff06ae22cfd16f37b7c5fad0bdbf62
bcyc <brianchancy1105@gmail.com>
2021-06-17 00:16:41 +0000

    [FIX] Change debug msg and lend_rate.

--- python/lending.py
+++ python/lending.py
@@ -4,24 +4,22 @@ from traceback import format_exc
 
 LEND_COINS = ['ETH', 'USD']
 
-
 def auto_lending(client: FtxClient) -> None:
     print('[auto_lending] DEBUG: {}'.format(datetime.datetime.now()))
     for coin in LEND_COINS:
         print('[auto_lending] DEBUG: start lending {}'.format(coin))
         lend_coin(client, coin)
 
-
 def lend_coin(client: FtxClient, coin: str) -> None:
     print('[lend_coin] DEBUG: {}'.format(datetime.datetime.now()))
     balance_coin = client.get_margin_lending_info_coin(coin)
     if balance_coin is None:
         return
-    print(balance_coin)
+    print('[lend_coin] DEBUG: {}'.format(balance_coin))
     available_balance = float(format(balance_coin['lendable'], '.6f')) - 0.000001
     lend_rate = client.get_margin_lending_rate_coin(coin)
-    print(lend_rate)
-
+    print('[lend_coin] DEBUG: {}'.format(lend_rate))
+    lend_rate['estimate'] -= 0.000001
     if available_balance > 0:
         print('Lending coin: {}, size: {}, rate: {}'.format(coin, available_balance, lend_rate['estimate']))
         try:
