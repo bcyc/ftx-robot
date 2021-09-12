a9a3446e5e0030de31cb4320531880afc4dba074
bcyc <brianchancy1105@gmail.com>
2021-09-12 17:23:10 +0900

    [FIX] Change the balance value and print message

--- python/staking.py
+++ python/staking.py
@@ -11,11 +11,11 @@ def stake_coin(client: FtxClient, coin: str) -> None:
     balance_coin = client.get_balance_coin(coin)
     if balance_coin is None:
         return
-    available_balance = balance_coin['availableWithoutBorrow'] - 0.00000001
+    available_balance = balance_coin['availableWithoutBorrow']
     print('[stake_coin] DEBUG: balance = {}'.format(balance_coin))
     if available_balance > 0:
         try:
             print('[stake_coin] DEBUG: coin = {}, size = {}'.format(coin, available_balance))
-            client.stakes(coin=coin, size=available_balance)
+            print(client.stakes(coin=coin, size=available_balance))
         except:
             print(format_exc())
